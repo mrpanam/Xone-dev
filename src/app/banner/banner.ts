2c7fa41ef0f6097d2b2280fe695fdb909a095022
eric <mrpanam.p@gmail.com>
2025-09-17 07:23:00 +0200

    v2.7

---
+++ src/app/banner/banner.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { User } from '../models/user.model';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-banner',
+  imports: [CurrencyPipe, DecimalPipe],
+  templateUrl: './banner.html',
+  styleUrl: './banner.css'
+})
+export class Banner {
+
+    user: User = {
+      id: 'user123',
+      name: 'Eric Paris',
+      email: 'john.doe@example.com',
+      avatar: 'https://ui-avatars.com/api/?name=Eric+Paris&background=000000&color=fff&size=128',
+      balance: 76756.75,
+      level: 24,
+      rating: 4.8,
+      tradesCompleted: 156
+    };
+
+}
